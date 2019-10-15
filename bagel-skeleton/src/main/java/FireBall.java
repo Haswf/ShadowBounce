dce32c9c593369113acbf50b6b0efc4833568b3b
Haswell <shuyangf@student.unimelb.edu.au>
2019-10-15 14:46:55 +1100

    Add JavaDoc

--- bagel-skeleton/src/main/java/FireBall.java
+++ bagel-skeleton/src/main/java/FireBall.java
@@ -1,8 +1,16 @@
 import bagel.Image;
-import bagel.util.Point;
 
+/**
+ *
+ */
 public class FireBall extends Ball {
+    // The range of explosion in pixels within which all pegs of the struck peg’s centre are destroyed.
     public final static int DESTROY_RANGE = 70;
+
+    /**
+     * Constructor to convert any ball into a fireball
+     * @param other
+     */
     public FireBall(Ball other){
         super(other.getCenter(), new Image("res/fireball.png"), other.velocity());
     }
--- bagel-skeleton/src/main/java/Powerup.java
+++ bagel-skeleton/src/main/java/Powerup.java
@@ -10,29 +10,37 @@ import java.util.Random;
 import java.util.logging.Level;
 
 public class Powerup extends GameObject implements Movable, OnCollisionCreate, OnCollisionRemove {
+    // The chance that a powerup will be created at the start of each turn.
     private static final double CHANCE = 0.1;
+    // The minimum distance from destination
     private static final int MIN_DISTANCE = 5;
+    // Initial speed of the powerup
     private static final int INIT_SPEED = 3;
+    // velocity of the powerup
     private Vector2 velocity;
+    // Current destination
     private Point destination;
 
     /**
-     *
+     * Poweru[ Constructor
      */
     public Powerup(){
+        // Choose a random point on the screen as position
         super(nextPoint(), new Image("res/powerup.png"));
+        // Choose another point as destination
         destination =nextPoint();
         this.velocity = destination.asVector().sub(getCenter().asVector()).normalised().mul(INIT_SPEED);
     }
 
     /**
-     *
-     * @return
+     * Gets a random point on the screen
+     * @return Point a random point on the screen
      */
     private static Point nextPoint(){
         Random random = new Random();
         return new Point(random.nextInt(Window.getWidth()), random.nextInt(Window.getHeight()));
     }
+
 
     /**
      *
@@ -49,8 +57,8 @@ public class Powerup extends GameObject implements Movable, OnCollisionCreate, O
     }
 
     /**
-     *
-     * @return
+     * Gets the velocity of the powerup
+     * @return a Vector2 representing current velocity
      */
     @ Override
     /* Return a  object representing current movement of the object. */
@@ -59,12 +67,17 @@ public class Powerup extends GameObject implements Movable, OnCollisionCreate, O
     }
 
     /**
+     * Move this powerup to its current destination.
+     * When the powerup is within 5 pixels of its destination,
+     * it will choose another random position as new destination.
      *
      */
     @ Override
     public void move(){
+        // Move the object based on its velocity
         setCenter(this.getCenter().asVector().add(velocity).asPoint());
 
+        // choose next destination if the the powerup is within 5 pixels of its destination
         if (this.distance(this.destination) < MIN_DISTANCE){
             destination = nextPoint();
             this.velocity = destination.asVector().sub(getCenter().asVector()).normalised().mul(INIT_SPEED);
@@ -72,8 +85,8 @@ public class Powerup extends GameObject implements Movable, OnCollisionCreate, O
     }
 
     /**
-     *
-     * @return
+     * Return the object to be removed when this powerup was hit. In this case, itself should be removed.
+     * @returm the GameObject to be removed as the result of a collision.
      */
     @ Override
     public GameObject onCollisionRemove(){
@@ -97,9 +110,9 @@ public class Powerup extends GameObject implements Movable, OnCollisionCreate, O
     }
 
     /**
-     *
-     * @param incoming
-     * @return
+     * Activate the powerup and replaces the incoming ball with a fire ball.
+     * @param incoming incoming ball
+     * @return Fireball a fire ball with the same velocity as the incoming one.
      */
     private FireBall activate(Ball incoming){
         return new FireBall(incoming);
